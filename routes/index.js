8628f8e9d71100fe0a5b2b08f7bac946bee56198
Noel Johansson <noel.johansson@elev.ga.ntig.se>
2024-01-25 11:17:43 +0100

    databas

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,10 @@ router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome', part: story.parts[0], username: req.session.username })
 })
 
-router.post('/username', function (req, res){
-req.session.username= req.body.username
-console.log(req.session.username)
-res.redirect('/')
+router.post('/username', function (req, res) {
+  req.session.username = req.body.username
+  console.log(req.session.username)
+  res.redirect('/')
 
 })
 
@@ -25,45 +25,46 @@ router.get('/story/:id', function (req, res) {
   res.render('part.njk', { title: part.name, part: part })
 
 
-const name = part.name.replace('[PLAYER]' , req.session.username)
-console.log(part)
-part={...part, name: name}
-console.log(part)
-res.render('part.njk' , {title: name, part: part})
+  const name = part.name.replace('[PLAYER]', req.session.username)
+  console.log(part)
+  part = { ...part, name: name }
+  console.log(part)
+  res.render('part.njk', { title: name, part: part })
 })
 
 router.get('/dbtest/:id', async (req, res) => {
-try {
-  const id =req.params.id
+  try {
+    const id = req.params.id
 
-  const [parts] = await pool
+    const [parts] = await pool
 
-  .promise()
+      .promise()
 
-  .query(`SELECT * FROM noel_part WHERE id = ${id}`)
+      .query(`SELECT * FROM noel_part WHERE id = ${id}`)
 
-  const [options] = await pool
+    const [options] = await pool
 
-  .promise()
+      .promise()
 
-  .query(`SELECT * FROM noel_option WHERE id = ${id}`)
-  const part = {...parts[0], options}
-  
-res.render('part.njk'),{
+      .query(`SELECT * FROM noel_option WHERE id = ${id}`)
+    const part = { ...parts[0], options } 
+    console.log(part)
 
-  username: req.session.username,
+    res.render('part.njk'), {
 
-  title: part.name,
+      username: req.session.username,
 
-  part,
-}
+      title: part.name,
 
-}
-catch (error){
+      part, part,
+    }
 
-  console.log (error)
-  res.sendStatus(500)
-}
+  }
+  catch (error) {
+
+    console.log(error)
+    res.sendStatus(500)
+  }
 
 }
 
